9f92dcebc73a6bde0d654f825ce3e7463a21f43f
sanseyooyea <1187586838@qq.com>
2024-01-17 04:34:25 +0800

    move packet to java

--- src/main/java/com/github/sanseyooyea/pyocd/services/MyProjectService.kt
+++ src/main/java/com/github/sanseyooyea/pyocd/services/MyProjectService.kt
@@ -1,9 +1,9 @@
 package com.github.sanseyooyea.pyocd.services
 
+import com.github.sanseyooyea.pyocd.MyBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.sanseyooyea.pyocd.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/java/com/github/sanseyooyea/pyocd/toolWindow/MyToolWindowFactory.kt
+++ src/main/java/com/github/sanseyooyea/pyocd/toolWindow/MyToolWindowFactory.kt
@@ -1,5 +1,7 @@
 package com.github.sanseyooyea.pyocd.toolWindow
 
+import com.github.sanseyooyea.pyocd.MyBundle
+import com.github.sanseyooyea.pyocd.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -8,8 +10,6 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.sanseyooyea.pyocd.MyBundle
-import com.github.sanseyooyea.pyocd.services.MyProjectService
 import javax.swing.JButton
 
 
